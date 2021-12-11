61a90ecb6c45a3edada4c32f7e9ddc9eb5c47750
Okelekele Nobel Bobby <bsoftlimited@gmail.com>
2021-12-11 04:40:16 +0100

    Just created the starting template

---
+++ desktop/src/com/bsoft/color/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bsoft.color.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bsoft.color.Color;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Color(), config);
+	}
+}
